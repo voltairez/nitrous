b0e1a87f7106f19067ac5bc8957f0b60bda81ff1
Voltair <voltairez@users.noreply.github.com>
2019-07-16 12:01:12 +0800

    disable git version reading

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const git = require('git-rev-sync');
+// const git = require('git-rev-sync');
 const baseConfig = require('./base.config');
 
 module.exports = {
@@ -9,7 +9,7 @@ module.exports = {
             'process.env': {
                 BROWSER: JSON.stringify(true),
                 NODE_ENV: JSON.stringify('production'),
-                VERSION: JSON.stringify(git.long())
+                // VERSION: JSON.stringify(git.long())
             }
         }),
         new webpack.optimize.UglifyJsPlugin({
